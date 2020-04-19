ec7c57d87f1742891647a9285a1eadf1d150e84f
volantes <cvolantesilva@gmail.com>
2020-04-19 19:20:07 -0400

    better defined the SimSegmentedMemory api

--- angr/storage/segmented_memory.py
+++ angr/storage/segmented_memory.py
@@ -19,9 +19,14 @@ class Segment:
 
 class SimSegmentedMemory(SimPagedMemory):
     def __init__(self, memory_backer=None, permissions_backer=None, pages=None, initialized=None, name_mapping=None, hash_mapping=None, page_size=None, symbolic_addrs=None, check_permissions=False):
-        super().__init__(memory_backer, permissions_backer, pages, initialized, name_mapping, hash_mapping, page_size, symbolic_addrs, check_permissions)
+        super(SimSegmentedMemory, self).__init__(memory_backer, permissions_backer, pages, initialized, name_mapping, hash_mapping, page_size, symbolic_addrs, check_permissions)
+
+        self._memory_backer = { } if memory_backer is None else memory_backer
+        self._permissions_backer = permissions_backer # saved for copying
+        self._check_perms = check_permissions
 
         self._assoc_pointsto_sets = None
+
         # For points_to analyse, see analyses/points_to.py
         self._points_to = global_apis.POINTS_TO_ANALYSE
         l.debug(self._points_to)
@@ -36,4 +41,39 @@ class SimSegmentedMemory(SimPagedMemory):
         pass
 
     # Override other APIs here!
-
+    # def __getstate___(self):
+    #     return {
+    #         'state1': 'self.state1'
+    #     }
+    #
+    # def __setstate__(self, state):
+    #     pass
+    #
+    # def branch(self):
+    #     # more to do and copy but this is a start
+    #     m = SimSegmentedMemory(
+    #         memory_backer=self._memory_backer,
+    #         permissions_backer=self._permissions_backer,
+    #         check_permissions=self._check_perms
+    #     )
+    #     return m
+    #
+    # def __getitem___(self, addr):
+    #     pass
+    #
+    # def __setitem__(self, addr, v):
+    #     pass
+    #
+    # def load_objects(selfself, addr, num_bytes, ret_on_segv=False):
+    #     pass
+    #
+    # def store_memory_object(self, mo, overwrite=True):
+    #     pass
+    #
+    # def keys(self):
+    #     sofar = set()
+    #     sofar.update(self._memory_backer.keys())
+    #
+    #     #they traverse self._pages here
+    #
+    #     return sofar
